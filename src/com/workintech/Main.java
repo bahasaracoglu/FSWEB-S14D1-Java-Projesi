2b779d9a16a37ca549eba0447d6cb0477e0fdb0c
bahasaracoglu <bahasaracoglu@gmail.com>
2023-07-31 16:15:57 +0300

    HrManager created and tests completed

--- src/com/workintech/Main.java
+++ src/com/workintech/Main.java
@@ -2,6 +2,10 @@ package com.workintech;
 
 import com.workintech.circle.Circle;
 import com.workintech.circle.Cylinder;
+import com.workintech.employee.HRManager;
+import com.workintech.employee.JuniorDeveloper;
+import com.workintech.employee.MidDeveloper;
+import com.workintech.employee.SeniorDeveloper;
 import com.workintech.rectangle.Cuboid;
 import com.workintech.rectangle.Rectangle;
 
@@ -47,5 +51,43 @@ public class Main {
         System.out.println("cuboid.height= " + cuboid.getHeight());
 
         System.out.println("cuboid.volume= " + cuboid.getVolume());
+
+        System.out.println("------ Employee ------");
+
+        System.out.println("cuboid.width= " + cuboid.getWidth());
+
+        JuniorDeveloper junior = new JuniorDeveloper(1,"Baha Saraçoğlu",10000);
+        MidDeveloper mid = new MidDeveloper(2,"2nd Dude",20000);
+        SeniorDeveloper senior = new SeniorDeveloper(3,"3rd Dude",30000);
+        HRManager hrmanager = new HRManager(4,"HR Dude",25000);
+
+
+        junior.work();
+        System.out.println("junior.name= " + junior.getName() );
+        System.out.println("junior.id= " + junior.getId() );
+        System.out.println("junior.salary= " + junior.getSalary() );
+
+        mid.work();
+        System.out.println("mid.name= " + mid.getName() );
+        System.out.println("mid.id= " + mid.getId() );
+        System.out.println("mid.salary= " + mid.getSalary() );
+
+        senior.work();
+        System.out.println("senior.name= " + senior.getName() );
+        System.out.println("senior.id= " + senior.getId() );
+        System.out.println("senior.salary= " + senior.getSalary() );
+
+        System.out.println("hr.name= " + hrmanager.getName() );
+        System.out.println("hr.id= " + hrmanager.getId() );
+        System.out.println("hr.salary= " + hrmanager.getSalary() );
+
+        hrmanager.addEmployee(junior);
+        hrmanager.addEmployee(mid);
+        hrmanager.addEmployee(senior);
+
+        System.out.println("juniorDevs" + hrmanager.getJuniorDevelopers());
+        System.out.println("midDevs" + hrmanager.getMidDevelopers());
+        System.out.println("seniorDevs" + hrmanager.getSeniorDevelopers());
+
     }
 }
--- src/com/workintech/employee/HRManager.java
+++ src/com/workintech/employee/HRManager.java
@@ -2,32 +2,47 @@ package com.workintech.employee;
 
 import java.util.Arrays;
 
-public class HRManager extends Employee{
+public class HRManager extends Employee {
 
-    private JuniorDeveloper juniorDevelopers [];
-    private MidDeveloper midDevelopers [];
-    private SeniorDeveloper seniorDevelopers [];
+    private JuniorDeveloper juniorDevelopers[] = new JuniorDeveloper[0];
+    private MidDeveloper midDevelopers[] = new MidDeveloper[0] ;
+    private SeniorDeveloper seniorDevelopers[] = new SeniorDeveloper[0] ;
 
 
     public HRManager(int id, String name, float salary) {
         super(id, name, salary);
     }
+
     public void work() {
         System.out.println("HRManager is working now!");
         super.setSalary(30000);
     }
 
     public void addEmployee(JuniorDeveloper newDeveloper) {
-        this.juniorDevelopers = Arrays.copyOf(juniorDevelopers,juniorDevelopers.length+1);
-        juniorDevelopers[juniorDevelopers.length] = newDeveloper;
+        this.juniorDevelopers = Arrays.copyOf(juniorDevelopers, juniorDevelopers.length + 1);
+        juniorDevelopers[juniorDevelopers.length - 1] = newDeveloper;
     }
+
     public void addEmployee(SeniorDeveloper newDeveloper) {
-        this.seniorDevelopers = Arrays.copyOf(seniorDevelopers,seniorDevelopers.length+1);
-        seniorDevelopers[seniorDevelopers.length] = newDeveloper;
+        this.seniorDevelopers = Arrays.copyOf(seniorDevelopers, seniorDevelopers.length + 1);
+        seniorDevelopers[seniorDevelopers.length - 1] = newDeveloper;
     }
+
     public void addEmployee(MidDeveloper newDeveloper) {
-        this.midDevelopers = Arrays.copyOf(midDevelopers,midDevelopers.length+1);
-        midDevelopers[midDevelopers.length] = newDeveloper;
+        this.midDevelopers = Arrays.copyOf(midDevelopers, midDevelopers.length + 1);
+        midDevelopers[midDevelopers.length - 1] = newDeveloper;
+    }
+
+    public JuniorDeveloper[] getJuniorDevelopers() {
+        return juniorDevelopers;
+    }
+
+    public MidDeveloper[] getMidDevelopers() {
+        return midDevelopers;
+    }
+
+    public SeniorDeveloper[] getSeniorDevelopers() {
+        return seniorDevelopers;
     }
 
 
